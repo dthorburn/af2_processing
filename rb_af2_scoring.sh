58030aa652d79545e8de2a5661f3bc7886ae6aab
dthorburn <miles.thorburn@gmail.com>
2026-01-12 11:24:24 +0000

    updated .sh template

--- rb_af2_scoring.sh
+++ rb_af2_scoring.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-export GOOGLE_APPLICATION_CREDENTIALS=/home/dthorbur/rb-main.json
+export GOOGLE_APPLICATION_CREDENTIALS=
 
-ProjectDir=/home/dthorbur/Resurrect_Bio/Projects/04_FloraFold/05_screen/af2_processing
+ProjectDir=
 cd $ProjectDir
 
 nextflow run rb_af2_scoring.nf \
     -c rb_af2_scoring.config \
     --Mode "pdbstats" \
-    --runname "hvulgare_rerun" \
-    --PS_input "gs://rb-interactions/hvulgare-bgraminis/florafold_rerun/predictions" \
-    --PS_output "/home/dthorbur/Resurrect_Bio/Projects/01_Candidate_Search/03_Interactions/07_hvulgare_bgraminis/florafold_rerun" \
-    --PS_nlrt_domains "/home/dthorbur/Resurrect_Bio/Scripts/rb_automation/af2_scoring/test_data/Glycine_max_nlrt_domains.tsv" \
-    --PI_regex_string '([A-Z0-9]+)_(EGZ[0-9]+)_(OG[0-9]+)_([0-9]+)'
+    --runname "example_name" \
+    --PS_input "path to local or GCP directory" \
+    --PS_output "path to local or GCP directory" \
+    --PS_nlrt_domains "path to specieis specific *_nlrt_domains.tsv (PATHOGEN output)" 
